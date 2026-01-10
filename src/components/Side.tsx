d1e30fb6e42712185eab35b6796db2432de0a1cd
UjjwalKumar02 <kumarujjwaldos@gmail.com>
2026-01-10 16:18:02 +0530

    fix deployment issue

--- src/components/Side.tsx
+++ src/components/Side.tsx
@@ -1,15 +1,16 @@
 import { Link, useNavigate } from "react-router-dom";
 
 export function Side() {
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const navigate = useNavigate();
   return (
     <div className="sticky top-20 bg-white flex flex-col gap-6 py-10 px-18 border border-gray-200 rounded-xl h-fit">
-      <Link to={"/dashboard"}>Home</Link>
+      <Link to={"/home"}>Home</Link>
       <Link to={"/profile"}>Profile</Link>
       <button
         className="text-start cursor-pointer"
         onClick={() => {
-          fetch("http://localhost:3000/api/user/logout", {
+          fetch(`${backendUrl}/api/user/logout`, {
             method: "POST",
             credentials: "include",
           }).then((res) => (res.ok ? navigate("/") : console.log(res.json())));
